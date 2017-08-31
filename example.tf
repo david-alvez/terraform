b16d1e45d0a490f19c50c97c350873ac4ca2e396
david-alvez <dalvez@gmail.com>
2017-08-31 13:57:42 +0200

    first commit

--- example.tf
+++ example.tf
@@ -1,6 +1,6 @@
 provider "aws" {
-  access_key = "AKIAI2EKUO45HMOK3EGA"
-  secret_key = "bl/wezMi1C6wZIGIWql/QR8hnGZEtrJoTCjCDznf"
+  access_key = ""
+  secret_key = ""
   region     = "eu-central-1"
 }
 
